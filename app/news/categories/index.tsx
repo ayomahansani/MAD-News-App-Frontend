dccfff42f4203562e64414e4e2e63bdde33f9f84
ayomahansani <ayohansi17@gmail.com>
2025-02-21 10:32:54 +0530

    create news folder, categories folder and index.tsx

---
+++ app/news/categories/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+function Categories(){
+    return(
+        <View>
+            <Text>Categories</Text>
+        </View>
+    );
+}
+
+export default Categories;
+
+const styles = StyleSheet.create({})

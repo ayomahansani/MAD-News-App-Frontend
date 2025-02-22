94e6fbde983f5a92ab834ac6ad6abb6ba07b0378
ayomahansani <ayohansi17@gmail.com>
2025-02-22 11:09:51 +0530

    add a list of countries to view in country category page

---
+++ app/news/countries/index.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from "react-native";
+import React from "react";
+
+function CountryNews() {
+    return (
+        <View>
+            <Text>CountryNews</Text>
+        </View>
+    );
+}
+
+export default CountryNews;
+
+const styles = StyleSheet.create({})

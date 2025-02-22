48a9a28da527af888df73ce5789298fe514f8581
ayomahansani <ayohansi17@gmail.com>
2025-02-22 08:51:30 +0530

    create Tab bar in bottom of the home page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,27 @@
 import {Tabs} from "expo-router";
+import {QueryClientProvider} from "@tanstack/react-query";
+import queryClient from "../lib/react-query/queryClient";
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function RootLayout() {
-    return <Tabs />
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Tabs>
+                <Tabs.Screen
+                    name="index"
+                    options={{
+                        title: "Home",
+                        headerShown: false,
+                        tabBarIcon: ({ color }) => (
+                            <MaterialIcons name="home" size={28} color={color} />
+                        ),
+                        tabBarLabelStyle: { fontSize: 12 },
+
+
+                    }}
+                />
+            </Tabs>
+        </QueryClientProvider>
+    );
+
 }

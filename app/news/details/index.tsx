facb3a3a1a95155ba74241cafc5245e02d23efb6
ayomahansani <ayohansi17@gmail.com>
2025-02-22 10:14:34 +0530

    create a details folder to show the news details when click a news card

---
+++ app/news/details/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+function NewsDetails(){
+    return(
+        <View>
+            <Text>NewsDetails</Text>
+        </View>
+    );
+}
+
+export default NewsDetails;
+
+const styles = StyleSheet.create({})
